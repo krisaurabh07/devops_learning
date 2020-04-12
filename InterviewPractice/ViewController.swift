97013d9ec7349713f071adf89c8a23e8212cea22
saurabh suman <saurabh.msuman@gmail.com>
2020-04-12 23:29:30 +0530

    Initial Commit

---
+++ InterviewPractice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  InterviewPractice
+//
+//  Created by saurabh suman on 12/04/20.
+//  Copyright © 2020 saurabh suman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
